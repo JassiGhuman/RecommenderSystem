f50b6f5c129a848fc0645398e044665343fdbda5
JassiGhuman <jaskiratsingh211@gmail.com>
2021-04-16 02:10:10 +0530

    Added HTML and CSS code

--- HRS/catalog/views.py
+++ HRS/catalog/views.py
@@ -3,11 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    """View function for home page of site."""
+    """View function for home page of site. Pass data to page"""
+    context = {
+    'num_books': 4,
+    'num_instances': 5,
+    'num_instances_available': 3,
+    'num_authors': 6,
+    }
 
     # Render the HTML template index.html with the data in the context variable.
-    return render(
-        request,
-        'index.html',
-        context={},
-    )
+    return render(request, 'index.html', context)
