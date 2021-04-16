570ddfe7ada60f7e08593fd6cd2831bb2717a7b5
JassiGhuman <jaskiratsingh211@gmail.com>
2021-04-16 22:35:29 +0530

    Update HTML and CSS code

--- HRS/catalog/urls.py
+++ HRS/catalog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('contact', views.contact, name='contact'),
+    path('about', views.about, name='about'),
 ]
--- HRS/catalog/views.py
+++ HRS/catalog/views.py
@@ -13,3 +13,28 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable.
     return render(request, 'index.html', context)
+    
+    
+def contact(request):
+    """View function for home page of site. Pass data to page"""
+    context = {
+    'num_books': 4,
+    'num_instances': 5,
+    'num_instances_available': 3,
+    'num_authors': 6,
+    }
+
+    # Render the HTML template index.html with the data in the context variable.
+    return render(request, 'contact.html', context)
+    
+def about(request):
+    """View function for home page of site. Pass data to page"""
+    context = {
+    'num_books': 4,
+    'num_instances': 5,
+    'num_instances_available': 3,
+    'num_authors': 6,
+    }
+
+    # Render the HTML template index.html with the data in the context variable.
+    return render(request, 'about.html', context)
