473659d92a6e078f512b7faebb78a37bdd9b6097
JassiGhuman <43839086+JassiGhuman@users.noreply.github.com>
2021-04-21 11:10:47 +0530

    Delete Visualization directory

--- Visualization/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class hotel(models.Model):
-    country = models.CharField(max_length=50)
-    city = models.CharField(max_length=50)
-    userid = models.CharField(max_length=50)
-    hotel = models.CharField(max_length=50)
